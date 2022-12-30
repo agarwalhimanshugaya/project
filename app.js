26127f138498c01b8e025aa2558954eeed3260ae
unknown <agarwalhimanshugaya@gmail.com>
2022-12-30 08:55:53 +0530

    salting and hashing

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const bodyparser=require("body-parser");
 const ejs=require("ejs");
 const mongoose=require("mongoose");
 const encrypt=require("mongoose-encryption");
+const md5=require("md5");
+const bcrypt=require("bcrypt");
+const saltround=10;
 const app=express();
 app.use(express.static("public"));
 app.set('view engine','ejs');
@@ -16,8 +19,6 @@ const userSchema=new mongoose.Schema({
     email: String,
     password: String
 });
-const secret="thisislittlesecretofalluser.";
-userSchema.plugin(encrypt,{secret:process.env.SECRET,encryptedFields:["password"]});
 const User=new mongoose.model("User",userSchema);
 app.get("/",function(req,res){
     res.render("home");
@@ -29,17 +30,20 @@ app.get("/register",function(req,res){
     res.render("register");
 });
 app.post("/register",function(req,res){
-    const newUser=new User({
-        email:req.body.username,
-        password:req.body.password
+    bcrypt.hash(req.body.password,saltround,function(err,hash){
+        const newUser=new User({
+            email:req.body.username,
+            password:hash
+        });
+        newUser.save(function(err){
+            if(err){
+                console.log(err);
+            }else{
+                res.render("secrets");
+            }
+        });
     });
-    newUser.save(function(err){
-        if(err){
-            console.log(err);
-        }else{
-            res.render("secrets");
-        }
-    });
+    
 });
 app.post("/login",function(req,res){
     const username=req.body.username;
@@ -49,9 +53,11 @@ app.post("/login",function(req,res){
             console.log(err);
         }else{
             if(foundUser){
-                if(foundUser.password==password){
-                    res.render("secrets");
-                }
+                bcrypt.compare(password,foundUser.password,function(err,result){
+                    if(result==true){
+                        res.render("secrets");
+                    }
+                });
             }
         }
     });
